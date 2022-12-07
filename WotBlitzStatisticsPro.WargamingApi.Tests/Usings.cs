4c44742209d757c012e576a58ab1f8682490fed5
Mikhail Shabanov <dr.mboga@yahoo.com>
2022-12-07 18:14:21 +0100

    feat: WotAPI tests

--- WotBlitzStatisticsPro.WargamingApi.Tests/Usings.cs
+++ WotBlitzStatisticsPro.WargamingApi.Tests/Usings.cs
@@ -1,1 +1,8 @@
 global using NUnit.Framework;
+global using System.Net;
+global using System.Text.Json;
+global using Moq;
+global using Moq.Protected;
+global using WotBlitzStatisticsPro.WargamingApi.Model;
+global using WotBlitzStatisticsPro.WargamingApi.Services;
+global using FluentAssertions;
