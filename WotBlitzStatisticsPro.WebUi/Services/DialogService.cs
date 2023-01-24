464f7efa8d33186a1c603f374947f8f808719971
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-01-24 11:01:13 +0100

    chore: modal showing mechanism

--- WotBlitzStatisticsPro.WebUi/Services/DialogService.cs
+++ WotBlitzStatisticsPro.WebUi/Services/DialogService.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using Microsoft.JSInterop;
 using WotBlitzStatisticsPro.WebUi.Messages;
 
 namespace WotBlitzStatisticsPro.WebUi.Services
@@ -6,10 +7,17 @@ namespace WotBlitzStatisticsPro.WebUi.Services
     public class DialogService :
         INotificationHandler<OpenTankDetailsNotification>
     {
-        public Task Handle(OpenTankDetailsNotification notification, CancellationToken cancellationToken)
+        private readonly IJSRuntime _jsRuntime;
+
+        public DialogService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task Handle(OpenTankDetailsNotification notification, CancellationToken cancellationToken)
         {
             Console.WriteLine($"Should open tank {notification.Tank.TankId}. But DialogService is not implemented yet");
-            return Task.CompletedTask;
+            await _jsRuntime.InvokeVoidAsync("modal.show", notification.Tank);
         }
     }
 }
---
+++ WotBlitzStatisticsPro.WebUi/wwwroot/scripts/modal.js
@@ -0,0 +1,6 @@
+var modal = modal || {};
+
+modal.show = function (data) {
+  var myModal = new bootstrap.Modal(document.getElementById('bootstrapModal'));
+  myModal.show();
+};
